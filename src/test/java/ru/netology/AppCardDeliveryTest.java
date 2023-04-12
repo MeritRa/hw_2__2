19470b01e24af33d8d88a474969b7037ed5401fd
Maria <melnikovamaria1708@gmail.com>
2023-04-12 18:36:23 +0500

    Initial commit

---
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AppCardDeliveryTest {
+    @Test
+    void shouldSendFormTest() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Ек");
+        $$(".menu-item").find(exactText("Екатеринбург")).click();
+        $("[data-test-id=date] input").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        $("[data-test-id=name] input").setValue("Иванов Андрей");
+        $("[data-test-id=phone] input").setValue("+79200000000");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
